cbeb6973d961f3927a8d6df6de24570224fcf3a0
iangifford <lightyagamie10@gmail.com>
2019-03-30 11:16:07 -0700

    framing tests

--- Lab04_TicTacToe/Lab04_TicTacToe/tictactoeTests/UnitTest1.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/tictactoeTests/UnitTest1.cs
@@ -1,27 +1,25 @@
 using System;
 using Xunit;
+using Lab04_TicTacToe.Classes;
 
 namespace tictactoeTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void TestForPlayerOneWin()
+        public void TestForPlayerWin()
         {
-
+            Player p1 = new Player();
+            Player p2 = new Player();
+            Game game = new Game(p1, p2);
         }
 
         [Fact]
-        public void TestForPlayerTwoWin()
+        public void TestForPlayerSwitch()
         {
-
+            
         }
 
-        [Fact]
-        public void TestForTurnSwitch()
-        {
-
-        }
 
         [Fact]
         public void TestForPlayerChoiceConfirmPosition()
