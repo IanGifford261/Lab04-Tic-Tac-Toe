648835ac9cc3faeb05cc956b04c301f07ec9aec4
iangifford <lightyagamie10@gmail.com>
2019-03-30 11:19:51 -0700

    First test passing

--- Lab04_TicTacToe/Lab04_TicTacToe/tictactoeTests/UnitTest1.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/tictactoeTests/UnitTest1.cs
@@ -12,25 +12,39 @@ namespace tictactoeTests
             Player p1 = new Player();
             Player p2 = new Player();
             Game game = new Game(p1, p2);
+
+            game.Board.GameBoard = new string[,]
+            {
+                {"X", "2", "3"},
+                {"4", "X", "6"},
+                {"7", "8", "X"},
+            };
+            Assert.True(game.CheckForWinner(game.Board));
         }
 
         [Fact]
         public void TestForPlayerSwitch()
         {
-            
+            Player p1 = new Player();
+            Player p2 = new Player();
+            Game game = new Game(p1, p2);
         }
 
 
         [Fact]
         public void TestForPlayerChoiceConfirmPosition()
         {
-
+            Player p1 = new Player();
+            Player p2 = new Player();
+            Game game = new Game(p1, p2);
         }
 
         [Fact]
         public void TestForDrawCondition()
         {
-
+            Player p1 = new Player();
+            Player p2 = new Player();
+            Game game = new Game(p1, p2);
         }
     }
 }
