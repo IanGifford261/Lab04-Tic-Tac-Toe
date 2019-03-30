a9c23abf0ed75ff5f1dc7b134d890e90058ef49a
iangifford <lightyagamie10@gmail.com>
2019-03-30 12:13:54 -0700

    tests written The play position test doesnt work when its a solo .equals. It should work. but didnt.

--- Lab04_TicTacToe/Lab04_TicTacToe/tictactoeTests/UnitTest1.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/tictactoeTests/UnitTest1.cs
@@ -41,6 +41,14 @@ namespace tictactoeTests
             Player p1 = new Player();
             Player p2 = new Player();
             Game game = new Game(p1, p2);
+
+            Position posi = new Position(0, 0);
+
+            //This should work, couldnt figure it out. Dan helped me break it out.
+            //Assert.Equal(posi, Player.PositionForNumber(1));
+
+            Assert.Equal(posi.Row, Player.PositionForNumber(1).Row);
+            Assert.Equal(posi.Column, Player.PositionForNumber(1).Column);
         }
 
         [Fact]
@@ -49,6 +57,14 @@ namespace tictactoeTests
             Player p1 = new Player();
             Player p2 = new Player();
             Game game = new Game(p1, p2);
+
+            game.Board.GameBoard = new string[,]
+{
+                {"X", "O", "O"},
+                {")", "X", "X"},
+                {"X", "X", "O"},
+};
+            Assert.False(game.CheckForWinner(game.Board));
         }
     }
 }
